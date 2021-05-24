e65c18af5c53f7485f4d41aaeabf6b110950c9d8
Milton Fernandez <miltonfernandez@Miltons-MacBook-Air.local>
2021-05-24 15:20:34 -0500

    first commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import {QUERIES} from '../../constants';
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
 
@@ -19,6 +20,19 @@ const App = () => {
 
 const Main = styled.main`
   padding: 64px 32px;
+
+  @media ${QUERIES.laptopAndSmaller} {
+  
+
+  };
+  @media ${QUERIES.tabletAndSmaller} {
+  
+
+  };
+  @media ${QUERIES.phoneAndSmaller} {
+   
+
+  };
 `;
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants';
 import Logo from '../Logo';
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -18,9 +21,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
+        </LogoWrapper>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -29,7 +32,27 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
-        <Side />
+        <MobileActions >
+          <UnstyledButton>
+            <Icon id ="shopping-bag">
+
+            </Icon>
+            <VisuallyHidden>open cart</VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id ="search">
+
+            </Icon>
+            <VisuallyHidden>search </VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id ="menu">
+
+            </Icon>
+            <VisuallyHidden>menu </VisuallyHidden>
+          </UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +69,37 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  border-top: 4px solid ${COLORS.gray[900]};
+  @media ${QUERIES.tabletAndSmaller}{
+    justify-content: space-between;
+    align-items:center;
+  }
+  @media ${QUERIES.phoneAndSmaller}{
+    padding-left:16px;
+    padding-right:16px;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+  @media ${QUERIES.tabletAndSmaller}{
+    display: none;
+  }
 `;
 
-const Side = styled.div`
+const LogoWrapper = styled.div`
   flex: 1;
+  @media ${QUERIES.tabletAndSmaller}{
+    flex:revert;
+  }
+`;
+const Filler = styled.div`
+  flex: 1;
+  @media ${QUERIES.tabletAndSmaller}{
+    display:none;
+  }
 `;
 
 const NavLink = styled.a`
@@ -69,5 +113,17 @@ const NavLink = styled.a`
     color: ${COLORS.secondary};
   }
 `;
+
+const MobileActions = styled.div`
+display:none;
+@media ${QUERIES.tabletAndSmaller}{
+  gap: 32px;
+  display:flex;
+}
+@media ${QUERIES.tabletAndSmaller}{
+  gap: 16px;
+}
+
+`
 
 export default Header;
